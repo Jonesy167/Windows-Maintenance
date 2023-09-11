4d29630dcf16a81c92e97486a3652342cfd80dab
Jonesy167 <30509908+Jonesy167@users.noreply.github.com>
2023-09-11 23:47:15 +0300

    Maintainance_ v2-1.bat

--- maintainance.bat
+++ maintainance.bat
@@ -1,9 +1,10 @@
-
 @echo off
 if not "%1"=="am_admin" (
     powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'" #check script is ran as admin
     exit /b
 )
+echo.
+echo.
 START "" cleanmgr /verylowdisk /c #cleanup c
 echo.
 echo.
@@ -11,6 +12,9 @@ PowerShell.exe -NoProfile -Command Clear-RecycleBin -Confirm:$false #empty Recyc
 echo.
 echo.
 defrag c: -f
+echo.
+echo.
+PowerShell.exe -NoProfile SFC /scannow #scan and check system files, auto fixes issues
 echo.
 echo.
 del %temp%\*.* /s /q #delete temp files
