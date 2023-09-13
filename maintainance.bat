40dd3c4f3f10a1c8fac70abb44ded5f928cb1b8f
Jonesy167 <30509908+Jonesy167@users.noreply.github.com>
2023-09-13 12:28:24 +0300

    Maintainance.bat

--- maintainance.bat
+++ maintainance.bat
@@ -3,28 +3,31 @@ if not "%1"=="am_admin" (
     powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'" #check script is ran as admin
     exit /b
 )
-echo.
-echo.
-START "" cleanmgr /verylowdisk /c #cleanup c
-echo.
-echo.
+
+echo y |PowerShell.exe Install-Module -name PSWindowsUpdate #install PSWindowsUpdate to enable us to use powershell to update windows
+
+powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted #required to enable loading of modules
+
+powershell Import-Module -Name PSWindowsUpdate # load PSWindowsUpdate module
+
+powershell Get-WindowsUpdate -AcceptAll -Install
+
+echo y |PowerShell.exe Install-Module PSWindowsUpdate -force #install powershell windows update module
+
 PowerShell.exe -NoProfile -Command Clear-RecycleBin -Confirm:$false #empty RecycleBin
-echo.
-echo.
+
+START "" cleanmgr /verylowdisk /c #cleanup c
+
 defrag c: -f
-echo.
-echo.
+
+del %temp%\*.* /s /q #delete temporary files
+
 PowerShell.exe -NoProfile SFC /scannow #scan and check system files, auto fixes issues
-echo.
-echo.
-del %temp%\*.* /s /q #delete temp files
-echo.
-echo.
+
 cipher /w:c: #delete unallocated
-echo.
-echo.
-echo MAINTAINANCE COMPLETE
-echo.
-echo.
+
+echo MAINTAINANCE COMPLETE REBOOTING TO COMPLETE WINDOWS UPDATES
+
 timeout /t 10 
-exit
+
+shutdown /r # reboot pc
